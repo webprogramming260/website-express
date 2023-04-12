71ceee16528d43fc1e729cc40fe9ff5921edcd2d
Lee <lee.jensen@agilix.com>
2023-04-12 11:35:15 -0600

    enhance() parameterized the name of the service that is displayed

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express();
 // The service port defaults to 3000 or is read from the program arguments
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
+// Text to display for the service name
+const serviceName = process.argv.length > 3 ? process.argv[3] : 'website';
+
 // Serve up the static content
 app.use(express.static('public'));
 
 // Provide the version of the application
-app.get('/version', (_req, res) => {
-  res.send({ version: '20221228.075705.1' });
+app.get('/config', (_req, res) => {
+  res.send({ version: '20221228.075705.1', name: serviceName });
 });
 
 // Return the homepage if the path is unknown
